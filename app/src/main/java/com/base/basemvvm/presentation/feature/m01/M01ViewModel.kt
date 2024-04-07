b1e927e2b5fa91521d023d4f86a2ac880b35bd34
vuxbasnin <banincora@gmail.com>
2024-04-07 12:07:12 +0700

    [Base MVVM] refactor

--- app/src/main/java/com/base/basemvvm/presentation/feature/m01/M01ViewModel.kt
+++ app/src/main/java/com/base/basemvvm/presentation/feature/m01/M01ViewModel.kt
@@ -1,9 +1,10 @@
 package com.base.basemvvm.presentation.feature.m01
 
 import com.base.basemvvm.data.DemoRepository
+import com.base.basemvvm.presentation.core.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class M01ViewModel @Inject constructor(private val demoRepository: DemoRepository) {
+class M01ViewModel @Inject constructor(private val demoRepository: DemoRepository): BaseViewModel() {
 }
